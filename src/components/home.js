6870ab4ff13e3b50755aed25c20fb29486661761
GunnDroid <glukari@protonmail.com>
2023-05-09 17:26:52 -0300

    new bio

--- src/components/home.js
+++ src/components/home.js
@@ -7,26 +7,31 @@ const Home = () => (
   <main>
     {/* Main Landing Content */}
     <p className="text-black text-4xl text-center pt-14 p-10 md:p-20 font-bold">
-      Who is Gunn Lukari?
+      Who is Gunnar Lukari?
     </p>
     <div className="grid md:grid-cols-2">
-      <p className="text-black text-xl md:w-5/6 mx-auto px-12 py-6 md:px-20 text-justify md:text-left order-2 md:order-1">
-        Gunnar is a skilled programmer from the United States. He discovered a
-        passion for programming through his interest in Smart Contracts. With a
-        strong desire to understand the intricacies of code, he set out on a
-        journey to master the craft.
+      <p className="text-black text-xl md:w-full mx-auto px-12 py-6 md:px-20 text-justify md:text-left order-2 md:order-1">
+        Gunnar is a dedicated full-stack developer based in the United States,
+        with a passion for programming ignited by an early interest in Smart
+        Contracts. He excels in frontend technologies like HTML, CSS, Tailwind,
+        JavaScript, and React, creating visually appealing and responsive user
+        interfaces. In backend development, Gunnar is proficient with tools like
+        Git and WordPress, ensuring robust and well-integrated functionality for
+        his projects.
         <br />
         <br />
-        Over the years, Gunnar diligently climbed the programming mountain,
-        mastering the fundamental building blocks of HTML, CSS, Tailwind,
-        Javascript, React, GIT, Wordpress and other essential technologies. He
-        completed several intermediate-level online courses to further enhance
-        his skills and knowledge.
+        Committed to staying current with the ever-changing tech landscape,
+        Gunnar actively engages in ongoing learning by completing a variety of
+        online courses. This dedication to continuous growth keeps him at the
+        forefront of emerging trends and best practices in both frontend and
+        backend development.
         <br />
         <br />
-        Gunnar is happy to contribute to the programming community and make a
-        meaningful impact in the field. He is well-prepared and ready to meet
-        the challenges that lie ahead.
+        Gunnar takes pride in contributing to the programming community and is
+        eager to make a positive impact within the industry. He approaches each
+        new challenge with enthusiasm and a problem-solving mindset, delivering
+        innovative solutions and top-notch programming expertise to every
+        project he undertakes.
         <br />
         <br />
       </p>
